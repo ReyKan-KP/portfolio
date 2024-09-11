9da03eed6537b3b4341f401cba0b74b61ae2f0b2
Kanishaka Pranjal <kanishakpranjal@gmail.com>
2024-09-12 00:30:17 +0530

    some fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
 "use client";
 import React, { useState, useEffect } from "react";
-// import { Header } from "@components/sections/Header";
-// import { HeroSection } from "@components/sections/Hero";
-// import { ProjectsSection } from "@components/sections/Projects";
-// import { TapeSection } from "@components/sections/Tape";
-// import { AboutSection } from "@components/sections/About";
+
 import { ContactSection } from "@components/sections/Contact";
-// import AchievementsList from "@components/sections/Achievements";
+
 import { Header } from "@sections/Header";
 import { HeroSection } from "@sections/Hero";
 import { ProjectsSection } from "@sections/Projects";
