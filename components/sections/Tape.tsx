3f47d253008cf0541b6d142fbc3f51aa98b3b9e4
Kanishaka Pranjal <kanishakpranjal@gmail.com>
2024-09-12 00:34:00 +0530

    some fixes

---
+++ components/sections/Tape.tsx
@@ -0,0 +1,45 @@
+import StarIcon from "@public/assets/icons/star.svg";
+import { Fragment } from "react";
+const words = [
+  "Performant",
+  "Accessible",
+  "Secure",
+  "Interactive",
+  "Scalable",
+  "User Friendly",
+  "Responsive",
+  "Maintainable",
+  "Search Optimized",
+  "Usable",
+  "Reliable",
+];
+export const TapeSection = () => {
+  return (
+    <div className="py-16 lg:py-24 overflow-x-clip">
+      <div className="bg-gradient-to-r from-emerald-300 to-sky-400  -rotate-3 -mx-1">
+        <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div
+            className="flex flex-none gap-4 py-3 pr-4 animate-move-left [animation-duration:30s] [animation-delay:1s]"
+            // style={{
+            //   transform: "rotate(-3deg)",
+            //   marginLeft: "-1rem",
+            // }}
+          >
+            {[...new Array(2)].fill(0).map((_, index) => (
+              <Fragment key={index}>
+                {words.map((word) => (
+                  <div key={word} className="inline-flex gap-4 items-center">
+                    <span className="text-gray-900 uppercase font-extrabold text-sm">
+                      {word}
+                    </span>
+                    <StarIcon className="size-6 text-gray-900 -rotate-12" />
+                  </div>
+                ))}
+              </Fragment>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
